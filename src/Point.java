3bca5e414a09f28806b5633b27103d22e4fa76f2
BT_1E10_04 <sample@bths.edu>
2022-04-01 13:37:34 -0400

    Initial commit

---
+++ src/Point.java
@@ -0,0 +1,30 @@
+public class Point
+{
+    private int x;
+    private int y;
+    private String label;
+
+    public Point(String label, int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+        this.label = label;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Point " + label + " is at x = " + x + ", y = " + y;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj == null || !(obj instanceof Point))
+        {
+            return false;
+        }
+        boolean good = (x == ((Point)obj).x) && (y == ((Point)obj).y);
+        return good;
+    }
+}
